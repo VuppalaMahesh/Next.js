4aaeac32815c17c09eaaeb0492f6908d2e6e1e11
unknown <mahesh.aina@gmail.com>
2026-06-22 14:41:23 +0530

    Added Counter, Loader, Routing

---
+++ app/components/Counter.js
@@ -0,0 +1,10 @@
+'use client';
+import { useState } from "react";
+export default function Counter({users}) {
+const [count, setCount] = useState(0);
+    return (
+    <div>
+        <button onClick={()=>setCount(c =>c+1)}>{count}</button>
+        <p>There are {users.length} users</p>
+    </div>
+)}
