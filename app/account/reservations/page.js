89fee47b7ec0a19b8f88da16b80d29b9bf71be48
VuppalaMahesh <mahicoolboy96@gmail.com>
2026-07-02 09:26:37 +0530

    feat: implement side navigation with menu links

---
+++ app/account/reservations/page.js
@@ -0,0 +1,16 @@
+import ReservationCard from "@/app/_components/ReservationCard";
+
+export const metadata = {
+  title: "Reservations",
+};
+
+export default function Page() {
+  // CHANGE
+  const bookings = [];
+
+  return (
+    <div>
+      Welcome, Mahesh
+    </div>
+  );
+}
